4e742ca962c4d4bb8b92e924ae4bd51407b24ce6
rahulyadavcalling <rahul.police86@gmail.com>
2016-09-22 09:09:31 +0530

    Add files via upload

---
+++ WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary1;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        [HttpGet]
+        public Customer1 GetCustomer(int id)
+        {
+            CustReposit rep = new CustReposit();
+            Customer1 c = rep.FindCustomer(id);
+            return c;
+        }
+        [HttpPost]
+     public String AddCustomer(CUSTOMER cust)
+        {
+            CustReposit rep = new CustReposit();
+            int status = rep.AddCustomer(Convert.ToInt32(cust.CIF),cust.CUSTOMER_FNAME,cust.CUSTOMER_LNAME,
+                Convert.ToDateTime(cust.CUSTOMER_DOB),cust.CUSTOMER_ADDRESS,cust.CUSTOMER_EMAIL);
+                if(status>0)
+            {
+                return "Record Successfully Added";
+            }
+            return "Some error in adding customer";
+                 
+        }
+    }
+}
