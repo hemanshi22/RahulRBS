5c32fe2c9abb785a5bb0d7534f022bb9b9496ca4
rahulyadavcalling <rahul.police86@gmail.com>
2016-09-22 09:06:42 +0530

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+    public class CustReposit
+    {
+        public int AddCustomer(int cid, string cfname, string lname, DateTime cdob, string address,
+            string email)
+        {
+            Entities context = new Entities();
+            CUSTOMER c = new CUSTOMER { CIF = cid, CUSTOMER_FNAME = cfname, CUSTOMER_DOB = cdob,
+                CUSTOMER_ADDRESS = address, CUSTOMER_EMAIL = email };
+            context.CUSTOMERs.Add(c);
+            int status = context.SaveChanges();
+            return status;
+        }
+        public Customer1 FindCustomer(int cid)
+        {
+            Entities context = new Entities();
+            CUSTOMER cust = context.CUSTOMERs.Where(c => c.CIF == cid).SingleOrDefault();
+            Customer1 cust1 = new Customer1();
+            cust1.CUSTOMER_ADDRESS = cust.CUSTOMER_ADDRESS;
+            cust1.CUSTOMER_DOB = Convert.ToDateTime(cust.CUSTOMER_DOB);
+            cust1.CUSTOMER_FNAME = cust.CUSTOMER_FNAME;
+            cust1.CIF = cust.CIF;
+            cust1.CUSTOMER_PHONE = cust.CUSTOMER_PHONE;
+            cust1.CUSTOMER_EMAIL = cust.CUSTOMER_EMAIL;
+            return cust1;
+        }
+      
+  
+        
+    }
+}
---
+++ ClassLibrary1/Customer1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   public class Customer1
+    { 
+
+        public decimal CIF { get; set; }
+    public string CUSTOMER_TITLE { get; set; }
+    public string CUSTOMER_FNAME { get; set; }
+    public string CUSTOMER_LNAME { get; set; }
+    public DateTime CUSTOMER_DOB { get; set; }
+    public string CUSTOMER_EMAIL { get; set; }
+    public Nullable<decimal> CUSTOMER_PHONE { get; set; }
+    public string CUSTOMER_GENDER { get; set; }
+    public string CUSTOMER_ADDRESS { get; set; }
+    public string CUSTOMER_CITY { get; set; }
+    public string CUSTOMER_STATE { get; set; }
+    public Nullable<decimal> CUSTOMER_ZIPCODE { get; set; }
+    public string IDPROOF_TYPE { get; set; }
+    public Nullable<decimal> IDPROOF_NUMBER { get; set; }
+    public string ADDRESSPROOF_TYPE { get; set; }
+    public Nullable<decimal> ADDRESSPROOF_NUMBER { get; set; }
+
+}
+}
